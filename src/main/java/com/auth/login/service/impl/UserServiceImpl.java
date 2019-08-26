e942d51db00f8cca6d99bd64ed418ac41104635c
J-DK <dileepkumarjami@gmail.com>
2019-08-26 21:34:02 +0530

    fix(beans): registered mail sender and user service in IOC container

--- src/main/java/com/auth/login/service/impl/UserServiceImpl.java
+++ src/main/java/com/auth/login/service/impl/UserServiceImpl.java
@@ -13,8 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 public class UserServiceImpl implements UserService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
--- src/main/java/com/auth/login/util/MailSenderUtil.java
+++ src/main/java/com/auth/login/util/MailSenderUtil.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MailSenderUtil {
     private JavaMailSender javaMailSender;
 
